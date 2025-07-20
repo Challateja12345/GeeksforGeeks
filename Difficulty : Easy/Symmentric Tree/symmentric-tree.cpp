df96382fc8e7cc94bd8ca9173e383f9dbd434870
Challa Divya Teja Naga Venkata Prasad <22p31a1225@acet.ac.in>
2025-07-20 18:58:08 +0530

    Create symmentric-tree.cpp

---
+++ Difficulty : Easy/Symmentric Tree/symmentric-tree.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  bool isSym(Node* a, Node* b){
+    if(!a||!b) return a==b;
+    if(a->data!=b->data) return false;
+    return isSym(a->left,b->right)&&isSym(a->right,b->left);
+  }
+ public:
+    bool isSymmetric(Node* r){
+        return !r||isSym(r->left,r->right);
+    }
+};
