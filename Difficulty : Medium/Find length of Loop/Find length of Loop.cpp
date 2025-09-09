73f99a1c9bcab9028c4d3d02bbf97874b6f7af13
Challa Divya Teja Naga Venkata Prasad <22p31a1225@acet.ac.in>
2025-09-09 18:58:20 +0530

    Create Find length of Loop.cpp

---
+++ Difficulty : Medium/Find length of Loop/Find length of Loop.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLoop(Node* head) {
+        Node *s = head, *f = head;
+        while (f && f->next) {
+            s = s->next;
+            f = f->next->next;
+            if (s == f) {
+                int cnt = 1;
+                while (s->next != f) s = s->next, cnt++;
+                return cnt;
+            }
+        }
+        return 0;
+    }
+};
