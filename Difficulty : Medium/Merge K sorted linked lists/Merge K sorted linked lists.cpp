6cb55329716681bd08b2b34497907dd645d78542
Challa Divya Teja Naga Venkata Prasad <22p31a1225@acet.ac.in>
2025-09-09 18:55:53 +0530

    Create Merge K sorted linked lists.cpp

---
+++ Difficulty : Medium/Merge K sorted linked lists/Merge K sorted linked lists.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    Node* mergeKLists(vector<Node*>& arr) {
+        auto cmp = [](Node* a, Node* b) { return a->data > b->data; };
+        priority_queue<Node*, vector<Node*>, decltype(cmp)> pq(cmp);
+        
+        for (Node* head : arr) if (head) pq.push(head);
+        
+        Node dummy(-1), *tail = &dummy;
+        while (!pq.empty()) {
+            Node* node = pq.top(); pq.pop();
+            tail->next = node; tail = node;
+            if (node->next) pq.push(node->next);
+        }
+        return dummy.next;
+    }
+};
